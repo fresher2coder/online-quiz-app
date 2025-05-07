1e945c58078a0ce8a6f47ab3726f4c65429d692a
Dineshkumar Thangavel <sendmail2dk@gmail.com>
2025-05-07 14:21:32 +0530

    Server cors policy updated

--- online-quiz-app-backend/server.js
+++ online-quiz-app-backend/server.js
@@ -9,7 +9,13 @@ const userRoutes = require("./routes/user");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // use specific domain in production
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/quiz", quizRoutes);
